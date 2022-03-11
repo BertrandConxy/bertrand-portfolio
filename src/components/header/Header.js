19f61dd7c9a9d6e5563b5b23fd83adf4d70a2347
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-03-11 17:57:05 +0200

    Add image and social links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
+import myPhoto from '../../assets/profile1.JPG';
 
 const Header = () => (
-  <div>Header</div>
+  <header>
+    <div className="container header_container">
+      <h5>Hello I'm</h5>
+      <h1>Bertrand Mutangana</h1>
+      <h5 className="text-light">Full-stack Developer</h5>
+      <CTA />
+      <div className="me">
+        <img src={myPhoto} alt="my photo" />
+      </div>
+      <a href="#contact" className="scroll__down">Scroll Down</a>
+    </div>
+  </header>
 );
 
 export default Header;
---
+++ src/components/header/HeaderSocials.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HeaderSocials = () => (
+  <div className="heeader__socials">
+    <a href="https://linkedin.com" target="_blank" rel="noreferrer" />
+    <a href="https://github.com" target="_blank" rel="noreferrer" />
+    <a href="https://medium.com" target="_blank" rel="noreferrer" />
+    <a href="https://dribble.com" target="_blank" rel="noreferrer" />
+  </div>
+);
+
+export default HeaderSocials;
