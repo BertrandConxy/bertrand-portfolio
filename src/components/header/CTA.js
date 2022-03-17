7815e7ff62e88b4ef8d0846016c2f1586e974a04
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-03-17 18:10:17 +0200

    Add header section

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -3,8 +3,12 @@ import CV from '../../assets/cv.pdf';
 
 const CTA = () => (
   <div className="cta">
-    <a href={CV} download className="btn">Download CV</a>
-    <a href="#contact" className="btn btn-primary">Let's talk</a>
+    <a href={CV} download className="btn">
+      Download CV
+    </a>
+    <a href="#contact" className="btn btn-primary">
+      Let&lsquo;s talk
+    </a>
   </div>
 );
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './header.css';
 import CTA from './CTA';
-import myPhoto from '../../assets/profile1.JPG';
+import myPhoto from '../../assets/profile1-removebg-preview.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => (
   <header>
-    <div className="container header_container">
-      <h5>Hello I'm</h5>
+    <div className="container header__container">
+      <h5>Hello I&lsquo;m</h5>
       <h1>Bertrand Mutangana</h1>
       <h5 className="text-light">Full-stack Developer</h5>
       <CTA />
+      <HeaderSocials />
       <div className="me">
-        <img src={myPhoto} alt="my photo" />
+        <img src={myPhoto} alt="my pic" />
       </div>
-      <a href="#contact" className="scroll__down">Scroll Down</a>
+      <a href="#contact" className="scroll__down">
+        Scroll Down
+      </a>
     </div>
   </header>
 );
