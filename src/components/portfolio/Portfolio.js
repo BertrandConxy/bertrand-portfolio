48debe3ff3ade5444dc3cc119b410e6cacf948f9
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-08-20 18:34:00 +0200

    Correct linter errors

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -12,11 +12,11 @@ const projectData = [
   {
     id: 1,
     image: ProjectImage1,
-    title: "Stock Market Trends",
+    title: 'Stock Market Trends',
     description:
-      "Browse different active companies on the NewYork Stock Exchange Market, view company profile and past financial statement records. Built with React.js, Redux, React-testing-library, jest, bootstrap and slider.js",
-    github: "https://github.com/BertrandConxy/Stocks-market-trends",
-    demo: "https://stockmarkettrends.netlify.app/",
+      'Browse different active companies on the NewYork Stock Exchange Market, view company profile and past financial statement records. Built with React.js, Redux, React-testing-library, jest, bootstrap and slider.js',
+    github: 'https://github.com/BertrandConxy/Stocks-market-trends',
+    demo: 'https://stockmarkettrends.netlify.app/',
   },
 
   {
@@ -24,49 +24,49 @@ const projectData = [
     image: ProjectImage2,
     title: "Space Traveller's Hub",
     description:
-      "Browse available Space Rocket launch and dragons as announced by SpaceX corporation. Reserve on and join your favorite missions among the available. Built with React.js, Redux,React-testing-library, jest and bootstrap.",
-    github: "https://github.com/BertrandConxy/Space-Travellers-Hub",
-    demo: "https://spacecrafthub.netlify.app/",
+      'Browse available Space Rocket launch and dragons as announced by SpaceX corporation. Reserve on and join your favorite missions among the available. Built with React.js, Redux,React-testing-library, jest and bootstrap.',
+    github: 'https://github.com/BertrandConxy/Space-Travellers-Hub',
+    demo: 'https://spacecrafthub.netlify.app/',
   },
 
   {
     id: 3,
     image: ProjectImage3,
-    title: "Spent",
+    title: 'Spent',
     description:
-      "A personal finance tracker web app. Track your expenses and keep track of your budget. Built with Ruby, Ruby on Rails, devise, Rspec and bootstrap.",
-    github: "https://github.com/BertrandConxy/Spent",
-    demo: "https://cspent.herokuapp.com/",
+      'A personal finance tracker web app. Track your expenses and keep track of your budget. Built with Ruby, Ruby on Rails, devise, Rspec and bootstrap.',
+    github: 'https://github.com/BertrandConxy/Spent',
+    demo: 'https://cspent.herokuapp.com/',
   },
 
   {
     id: 4,
     image: ProjectImage4,
-    title: "Cinemash",
+    title: 'Cinemash',
     description:
-      "Browse different movies and find the detailed information about each. Like, comment and reserve your favorite on Cinemash. Built with Vanilla ES6 Javascript, Jest, Webpack, html and css.",
-    github: "https://github.com/BertrandConxy/JS-Capstone-Project",
-    demo: "https://cinemash.netlify.app/",
+      'Browse different movies and find the detailed information about each. Like, comment and reserve your favorite on Cinemash. Built with Vanilla ES6 Javascript, Jest, Webpack, html and css.',
+    github: 'https://github.com/BertrandConxy/JS-Capstone-Project',
+    demo: 'https://cinemash.netlify.app/',
   },
 
   {
     id: 5,
     image: ProjectImage5,
-    title: "Isomo Online",
-    github: "https://github.com/BertrandConxy/Isomo-online",
+    title: 'Isomo Online',
+    github: 'https://github.com/BertrandConxy/Isomo-online',
     description:
-      "This is E-learning platform where people can browse different courses available, enroll for getting the course and also get in touch with the teachers. Built with HTML, CSS and JavaScript.",
-    demo: "https://isomonline.netlify.app/",
+      'This is E-learning platform where people can browse different courses available, enroll for getting the course and also get in touch with the teachers. Built with HTML, CSS and JavaScript.',
+    demo: 'https://isomonline.netlify.app/',
   },
 
   {
     id: 6,
     image: ProjectImage6,
-    title: "Umuhanzi Concert Website",
-    github: "https://github.com/BertrandConxy/umuhanzi-concert-website",
+    title: 'Umuhanzi Concert Website',
+    github: 'https://github.com/BertrandConxy/umuhanzi-concert-website',
     description:
-      "Website for an even organising company where they can announce the upcoming event, provide all the details and people will be able to buy ticket on the site. Built with HTML, CSS and JavaScript.",
-    demo: "https://bertrandconxy.github.io/umuhanzi-concert-website/",
+      'Website for an even organising company where they can announce the upcoming event, provide all the details and people will be able to buy ticket on the site. Built with HTML, CSS and JavaScript.',
+    demo: 'https://bertrandconxy.github.io/umuhanzi-concert-website/',
   },
 ];
 
